6511531c6cf5cbf4a53a80551991e630369c173d
Lola Jones <lolajones2497@gmail.com>
2023-07-07 16:21:04 -0700

    layout contact page

--- src/pages/calc/Calc.js
+++ src/pages/calc/Calc.js
@@ -8,34 +8,37 @@ import { IoMdInformationCircleOutline } from "react-icons/io";
 export default function Calc() {
   return (
     <main>
+      <div className="lines"></div>
       <div className="ca">
         <div>
-          <h9 className="ggg">What is a Materials Calculator?</h9>
-          <h10 className="purpose">
+          <div className="ggg">What is a Materials Calculator?</div> <br></br>
+          <div className="purpose">
             The purpose of our materials calculator is to provide an estimated
             quantity of product(s) that is required for your project. To avoid
             shortage in product materials, we recommend to get 5-10% more
             product than the calculator recommends. However, it's important to
             note that variations in landscape elevation and other terrain
             factors can impact the accuracy of these results.
-          </h10>
+          </div>
         </div>
         <div>
           <h11 className="help">Tips to measuring: </h11>
-          <ul>
-            <li>
-              Measure your entire project in a square or rectangle for best
-              results.
-            </li>
-            <li>
-              When planning to cover an area with gravel account for the depth
-              of the area. Will you need more than 2" of depth?
-            </li>
-            <li>
-              Know the size of gravel or minus you need. The size and type of
-              the gravel will affect the required product quantity.
-            </li>
-          </ul>
+          <div className="helpans">
+            <ul>
+              <li>
+                Measure your entire project in a square or rectangle for best
+                results.
+              </li>
+              <li>
+                When planning to cover an area with gravel account for the depth
+                of the area. Will you need more than 2" of depth?
+              </li>
+              <li>
+                Know the size of gravel or minus you need. The size and type of
+                the gravel will affect the required product quantity.
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
       <div>

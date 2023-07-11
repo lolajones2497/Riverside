4ca63d857637aae87a0b14352230a09e1a04eea0
Lola Jones <lolajones2497@gmail.com>
2023-07-11 15:02:53 -0700

    design contact page

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -4,9 +4,9 @@ import Form from "./Form";
 
 export default function Contact() {
   return (
-    <div className="containersss">
+    <>
       <div className="lines"></div>
-      <div className="boxx">
+      <div className="container-contact">
         <div className="dump">
           <img
             src={Dumptruck}
@@ -14,24 +14,27 @@ export default function Contact() {
             className="dumptruck"
           />
         </div>
-        <div className="rightboxs">
+        <div className="right-container">
           <h11 className="ourstory">Our Story</h11>
           <h12 className="us">
-            Riverside Rock & Boulder was started back in 2002 with one truck and
-            a lot of family support. Our family has over 80 years of trucking
-            experience hauling landscape and aggregate materials. As we grew we
-            branched out and started spreading rock and installing full
-            landscape packages for homeowners and some commercial projects. We
-            weathered the recession and now have regained our footing and are
-            ready to provide our neighbors with landscape products and services
-            once again. Servicing: Phoenix, Tempe, Mesa, Gilbert, Chandler,
-            Apache Junction, Queen Creek, Ahwatukee San Tan Valley Arizona
+            Riverside Rock & Boulder was established in 2002 with humble
+            beginnings—a single truck and the invaluable support of our family.
+            With a combined experience of over 80 years in trucking,
+            specializing in hauling landscape and aggregate materials, we laid
+            the foundation for our journey. As our business grew, we expanded
+            our services to include rock spreading and comprehensive landscape
+            installations for both homeowners and select commercial projects. We
+            navigated through the challenges of the recession, and today, we
+            stand strong, ready to once again serve our community with
+            top-quality landscape products and services. Based in Arizona, we
+            proudly offer our expertise to the residents of Mesa, Chandler,
+            Queen Creek, Coolidge, and San Tan Valley.
           </h12>
         </div>
       </div>
       <div className="contactform">
         <Form />
       </div>
-    </div>
+    </>
   );
 }

108af0022f42b50ff2a4818c0bc7cf6b2774c24d
Lola Jones <lolajones2497@gmail.com>
2023-07-11 15:08:47 -0700

    update RRB navbar logo CSS

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,6 +32,7 @@ export default function Navbar() {
                 onClick={toggleInfo}
                 onMouseEnter={toggleInfo}
                 onMouseLeave={toggleInfo}
+                onMouseOver={toggleInfo}
               >
                 Products
               </div>
